dd72b0c1bac892ee56cc13ed2ebce9e389b0508e
leobaltazor <leo.baltazor@gmail.com>
2018-03-28 01:07:12 +0300

    ver 0.0.0.2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// для запуска используем команду::::=> npm run nodemon
 // Подключение express
 const express = require('express');
 // подключение bodyParser
@@ -5,33 +6,36 @@ const bodyParser = require('body-parser');
 
 // Создание объекта сервера
 let chat = express();
-chat.use(bodyParser.json())
+chat.use(bodyParser())
+// создаём Express-приложение
+let app = express();
 //all - get - post
 
 const ARRAY_RESPONSES = [];
 
-//request => req || response => res
-chat.get("/get-message", function(request, response) {
-	// console.log(request.query);
-	ARRAY_RESPONSES.push(response)
+//req => req || res => res
+chat.get("/get-message", function(req, res) {
+	// console.log(req.query);
+	ARRAY_RESPONSES.push(res)
 })
 
-chat.post("/send-message", function(request, response) {
-	let { message } = request.body;
+chat.post("/send-message", function(req, res) {
+	let { message } = req.body;
 	while (ARRAY_RESPONSES.length) {
 		const client = ARRAY_RESPONSES.pop()
 		client.send(`${message}`)
 	}
 	console.log("msg recive");
-	response.send("message send")
-	// response.send(message)
-
+	// res.send("message send")
+	// res.sendfile('index.html');
+	// res.send(message)
 })
-
-// chat.get("/index.html", function(req, res){
-// 	res.send(index.html)
-// })
 
 chat.listen(3000, function () {
 	console.log("run server 3000");
 })
+
+chat.get('/', function(req, res) {
+	res.sendfile('index.html');
+});
+console.log('Сервер стартовал!');
